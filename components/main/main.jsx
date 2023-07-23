dfe8bd0b0142734d7d901116bc0c5eebc686ebcc
Rover420 <100524342+Rover420@users.noreply.github.com>
2023-07-24 01:41:42 +0200

    Update main.jsx

--- components/main/main.jsx
+++ components/main/main.jsx
@@ -1,21 +1,8 @@
 import styles from './page.module.css'
 import Clicks from './clicks'
 
-async function getProviders() {
-  const res = await fetch('http://localhost:3000/api/auth/providers', { next: { revalidate: 60 } }) // The result is cached
-  return res.json()
-}
-
-const getClicks = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_NODE_URL}/clicks`, { next: { revalidate: 60 } });
-  return res.json();
-}
 
 const Main = async ({ t }) => {
-
-  const providers = await getProviders();
-
-  const prevClicks = await getClicks();
 
   return (
     <div style={{ height: '300vh' }}>
